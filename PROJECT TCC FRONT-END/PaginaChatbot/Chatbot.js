2e708c9ecbaa945dca41544dc166f05fbc61370e
duduusxz <eduardoataidemelo@gmail.com>
2025-05-17 00:23:42 -0300

    Menu meio caminho andado

--- PROJECT TCC FRONT-END/PaginaChatbot/Chatbot.js
+++ PROJECT TCC FRONT-END/PaginaChatbot/Chatbot.js
@@ -2,3 +2,23 @@ function abrirmenu() {
     const menu = document.getElementById("menu");
     menu.classList.toggle("ativo");
 }
+
+const menuContainer = document.querySelector(".menu-container");
+const menu = document.getElementById("menu");
+const addButton = document.querySelector(".add");
+
+menuContainer.addEventListener("mouseenter", () => {
+    menu.classList.add("ativo");
+});
+
+menuContainer.addEventListener("mouseleave", () => {
+    menu.classList.remove("ativo");
+});
+
+addButton.addEventListener("mouseenter", () => {
+    menu.classList.add("ativo");
+});
+
+addButton.addEventListener("mouseleave", () => {
+    menu.classList.remove("ativo");
+});
