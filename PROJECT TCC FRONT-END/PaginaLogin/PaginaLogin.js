2c2f7e6541cc926fca7beea030876f35c75b2afd
duduusxz <eduardoataidemelo@gmail.com>
2025-06-06 18:29:27 -0300

    Adicionando cadastro e esqueci a senha

--- PROJECT TCC FRONT-END/PaginaLogin/PaginaLogin.js
+++ PROJECT TCC FRONT-END/PaginaLogin/PaginaLogin.js
@@ -1,38 +1,32 @@
-function mostrarSenha() {
-  const input = document.getElementById("senha");
-  const icon = document.getElementById("toggleSenha");
-
-  if (input.type === "password") {
-    input.type = "text";
-    icon.classList.add("fa-eye");
-    icon.classList.remove("fa-eye-slash");
-  } else {
-    input.type = "password";
-    icon.classList.add("fa-eye-slash");
-    icon.classList.remove("fa-eye");
-
-  }
+function toggleMode() {
+    document.body.classList.toggle('light-mode');
+    document.body.classList.toggle('dark-mode');
 }
 
-function toggleMode() {
-            document.body.classList.toggle('light-mode');
-            document.body.classList.toggle('dark-mode');
-        }
+let texto = document.getElementById("texto-apresentacao-bem-vindo");
+let tamanhoBase = parseFloat(window.getComputedStyle(texto).fontSize);
 
-        let texto = document.getElementById("texto-apresentacao-bem-vindo");
-        let tamanhoBase = parseFloat(window.getComputedStyle(texto).fontSize);
+document.getElementById("increase-font").addEventListener("click", function () {
+let tamanhoAtual = parseFloat(window.getComputedStyle(texto).fontSize);
 
-        document.getElementById("increase-font").addEventListener("click", function () {
-            let tamanhoAtual = parseFloat(window.getComputedStyle(texto).fontSize);
-            texto.style.fontSize = (tamanhoAtual * 1.1) + "px"; // Aumenta 10%
-        });
+let novoTamanho = tamanhoAtual * 1.1;
 
-        document.getElementById("decrease-font").addEventListener("click", function () {
-            let tamanhoAtual = parseFloat(window.getComputedStyle(texto).fontSize);
-            if (tamanhoAtual > tamanhoBase * 0.8) { // Evita que fique muito pequeno
-                texto.style.fontSize = (tamanhoAtual * 0.9) + "px"; // Diminui 10%
-            }
-        });
+if (novoTamanho > 32) {
+novoTamanho = 32
+}
+
+texto.style.fontSize = novoTamanho + "px";
+});
+
+
+document.getElementById("decrease-font").addEventListener("click", function () {
+    let tamanhoAtual = parseFloat(window.getComputedStyle(texto).fontSize);
+    if (tamanhoAtual > tamanhoBase * 0.8) { // Evita que fique muito pequeno
+        texto.style.fontSize = (tamanhoAtual * 0.9) + "px"; // Diminui 10%
+    }
+
+    
+}); 
 
         const entrar = document.getElementById('entrar');
         const rm = document.getElementById('rm');
